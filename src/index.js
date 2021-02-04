df290f88eee251f6638f5ba06c7f8c647205236b
evilcore29 <evilcore29@gmail.com>
2021-02-04 17:13:27 +0300

    feat: implement brackets checker function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = str.split("");
+  const halfLength = arr.length;
+
+  const checkCondition = ({currentValue, nextValue}) => {
+    return (currentValue === "{" && nextValue === "}") ||
+      (currentValue === "[" && nextValue === "]") ||
+      (currentValue === "(" && nextValue === ")") ||
+      (currentValue === "|" && nextValue === "|") ||
+      (currentValue === "1" && nextValue === "2") ||
+      (currentValue === "3" && nextValue === "4") ||
+      (currentValue === "5" && nextValue === "6") ||
+      (currentValue === "7" && nextValue === "7") ||
+      (currentValue === "8" && nextValue === "8");
+  };
+  for (let i = 0; i < halfLength / 2; i++) {
+    for (let k = 0; k < arr.length; k++) {
+      if (checkCondition({currentValue : arr[k], nextValue: arr[k + 1]})) {
+        arr.splice(k, 2);
+      }
+    }
+  }
+
+  return arr.length === 0;
+};
